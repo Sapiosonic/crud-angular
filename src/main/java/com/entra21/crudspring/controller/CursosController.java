82486b943d7cccaca529a2b0ab0d65326d325888
Radamés Rodrigues <radames.idiomas@gmail.com>
2022-10-23 15:43:11 -0300

    Criar curso HTTP POST

--- src/main/java/com/entra21/crudspring/controller/CursosController.java
+++ src/main/java/com/entra21/crudspring/controller/CursosController.java
@@ -5,9 +5,14 @@ import java.util.List;
 import com.entra21.crudspring.model.Curso;
 import com.entra21.crudspring.repository.CursosRepository;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
@@ -24,5 +29,13 @@ public class CursosController {
         return cursosRepository.findAll();
     }
 
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Curso create(@RequestBody Curso curso){
+        //System.out.println(curso.getNome());
+        return cursosRepository.save(curso);
+        //return ResponseEntity.status(HttpStatus.CREATED).body(cursosRepository.save(curso));
+    }
 
 }
