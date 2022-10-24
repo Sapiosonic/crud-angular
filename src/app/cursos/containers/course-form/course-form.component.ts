5dcc0331c8383428381c9819a8dd35959459f659
Radamés Rodrigues <radames.idiomas@gmail.com>
2022-10-23 23:57:38 -0300

    "Atualizar curso e popular o formulário"

--- src/app/cursos/containers/course-form/course-form.component.ts
+++ src/app/cursos/containers/course-form/course-form.component.ts
@@ -2,6 +2,8 @@ import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, NonNullableFormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+import { Curso } from '../../model/curso';
 import { CursosService } from '../../services/cursos.service';
 
 @Component({
@@ -12,6 +14,7 @@ import { CursosService } from '../../services/cursos.service';
 export class CourseFormComponent implements OnInit {
 
   form = this.formBuilder.group({
+      _id: [''],
       nome: [''],
       categoria: ['']
     });
@@ -20,12 +23,18 @@ export class CourseFormComponent implements OnInit {
       private formBuilder: NonNullableFormBuilder,
       private service: CursosService,
       private snackBar: MatSnackBar,
-      private location: Location) { 
+      private location: Location,
+      private route: ActivatedRoute) { 
     //this.form 
   }
 
   ngOnInit(): void {
-    
+    const curso: Curso = this.route.snapshot.data['curso'];
+    this.form.setValue({
+      _id: curso._id,
+      nome: curso.nome,
+      categoria: curso.categoria
+    })
   }
 
   onSubmit(){
